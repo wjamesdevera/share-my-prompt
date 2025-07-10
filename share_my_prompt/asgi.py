622171ad888e244c0861b297a896f15d774e8b01
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-10 23:40:28 +0800

    Initialize django project

---
+++ share_my_prompt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for share_my_prompt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'share_my_prompt.settings')
+
+application = get_asgi_application()
