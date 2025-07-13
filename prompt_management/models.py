546ff5ace48d4155ea9b9c16aa942a271a768ba0
w.jamesdevera <w.jamesdevera@gmail.com>
2025-07-13 20:32:59 +0800

    Add: prompt model

--- prompt_management/models.py
+++ prompt_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import CharField
+import uuid
 
-# Create your models here.
+
+class Prompt(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title: str = models.CharField(max_length=255)
+    content: str = models.TextField()
+
+    def __str__(self):
+        return str(self.title)
